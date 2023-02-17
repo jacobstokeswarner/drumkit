5ec4452631d7e1cbe4a6664287676909ae84e27a
Jacob Stokes-Warner <jacobstokeswarner@gmail.com>
2023-02-17 15:48:52 -0600

    added functionality for playing different drum sounds on click

--- js/index.js
+++ js/index.js
@@ -17,7 +17,36 @@ for (var i = 0; i < buttons.length; i++) {
 
 function handleClick() {
     //this specifies the specific object that triggered the event
-    this.style.color = "yellow"
+    switch (this.innerHTML) {
+        case "w":
+            new Audio("sounds/tom-1.mp3").play();
+            break;
+        case "a":
+            new Audio("sounds/tom-2.mp3").play();
+            break;
+        case "s":
+            new Audio("sounds/tom-3.mp3").play();
+            break;
+        case "d":
+            new Audio("sounds/tom-4.mp3").play();
+            break;
+        case "j":
+            new Audio("sounds/snare.mp3").play();
+            break;
+        case "k":
+            new Audio("sounds/crash.mp3").play();
+            break;
+        case "l":
+            new Audio("sounds/kick-bass.mp3").play();
+            break;
+
+
+
+
+
+    }
+
+
   }
 
 
