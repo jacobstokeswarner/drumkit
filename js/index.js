12d3583da460e2177f9c5215e901b379b60d1435
Jacob Stokes-Warner <jacobstokeswarner@gmail.com>
2023-02-20 08:19:01 -0600

    refined my code to be less repetitive

--- js/index.js
+++ js/index.js
@@ -8,50 +8,27 @@
 
 // to add the event listener to all of the buttons:
 
+// detecting button click
 var buttons = document.querySelectorAll("button.drum");
 
 for (var i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", handleClick);
+  buttons[i].addEventListener("click", function(){
+    handlePress(this.innerHTML);
+  });
 }
 
 
-function handleClick() {
-    //this specifies the specific object that triggered the event
-    switch (this.innerHTML) {
-        case "w":
-            new Audio("sounds/tom-1.mp3").play();
-            break;
-        case "a":
-            new Audio("sounds/tom-2.mp3").play();
-            break;
-        case "s":
-            new Audio("sounds/tom-3.mp3").play();
-            break;
-        case "d":
-            new Audio("sounds/tom-4.mp3").play();
-            break;
-        case "j":
-            new Audio("sounds/snare.mp3").play();
-            break;
-        case "k":
-            new Audio("sounds/crash.mp3").play();
-            break;
-        case "l":
-            new Audio("sounds/kick-bass.mp3").play();
-            break;
-        default:console.log(this.innerHTML);
-    }
+
 
 // use keydown instead of keypress
-  }
 
-
-// var sound = new Audio("sounds/tom-1.mp3");
-// sound.play();
+// detecting keypress
 document.addEventListener("keydown", function(event){
     handlePress(event.key);
 });
 
+//this same function can be used for keypress and also button press events
+// due to the parameter
 function handlePress(key){
     switch (key) {
         case "w":
